b9e4f8cfe0629795db1798911007fb41b8c5e3eb
Theresa Abl <theresaabl@gmx.at>
2025-01-25 15:19:31 +0000

    Install whitenoise and set up serving of static files

--- sharedspaces/settings.py
+++ sharedspaces/settings.py
@@ -63,6 +63,7 @@ ACCOUNT_FORMS = {'signup': 'dashboard.forms.MyCustomSignupForm'}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -158,6 +159,7 @@ TIME_INPUT_FORMATS = [
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
