287222fed7d4eb16f0b8287668363ead6577b39e
Theresa Abl <theresaabl@gmx.at>
2025-01-25 11:40:14 +0000

    Create EventSpace model, set up cloudinary api

--- dashboard/models.py
+++ dashboard/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
-# Create your models here.
+
+class EventSpace(models.Model):
+    name = models.CharField(max_length=200, unique=True)
+    type = models.CharField(max_length=200)
+    image = CloudinaryField('image', default='placeholder')
+    building = models.CharField(max_length=200)
+    capacity = models.PositiveSmallIntegerField()
+    number_of_tables = models.PositiveSmallIntegerField()
+    number_of_chairs = models.PositiveSmallIntegerField()
+    kitchen = models.BooleanField()
+    tea_and_coffeemaker = models.BooleanField()
+    projector = models.BooleanField()
+    audio_equipment = models.BooleanField()
+    childrens_play_area = models.BooleanField()
+    piano = models.BooleanField()
+    notes = models.TextField(blank=True)
--- sharedspaces/settings.py
+++ sharedspaces/settings.py
@@ -44,13 +44,15 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'cloudinary_storage',
     'django.contrib.sites',
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
+    'cloudinary',
     'main',
     'contact',
-    'dashboard',  
+    'dashboard',
 ]
 
 SITE_ID = 1
