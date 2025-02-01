ccc03ba872925c120c6570696f7f35e5e9554453
theresaabl <theresaabl@gmx.at>
2025-02-01 19:25:29 +0100

    Add costum js for deleting and editing resident requests

--- static/js/edit_resident_requests.js
+++ static/js/edit_resident_requests.js
@@ -1,16 +1,16 @@
-const submitButton = document.getElementById("booking-submit-button");
-const title = document.getElementById("booking-title");
+const submitButton = document.getElementById("resident-request-submit-button");
+const title = document.getElementById("resident-request-title");
 const URL = window.location.href
 
 /**
-* Edit Bookings 
+* Edit Resident Requests
 * - Update title text
 * - Updates the submit button's text to "Update".
 */
 // check whether booking form is accessed through edit button
-if (URL.includes('edit_booking')){
+if (URL.includes('edit_resident_request')){
     addEventListener("DOMContentLoaded", (e) => {
-        title.innerText = "Edit Event Space Booking"
+        title.innerText = "Edit Request"
         submitButton.innerText = "Update";
     });
 }
