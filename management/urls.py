68e969e20063d3de78b5ec03c8c64c447878a87d
theresaabl <theresaabl@gmx.at>
2025-02-07 18:21:26 +0100

    Create view, url and template code to activate and deactivate users from the admin space

--- management/urls.py
+++ management/urls.py
@@ -4,4 +4,5 @@ from django.urls import path
 urlpatterns = [
     path('', views.management_page, name='management'),
     path('user-accounts/', views.user_accounts, name='users'),
+    path('user-accounts/(de)-activate/<int:user_id>', views.user_activation, name='user-activation'),
 ]
--- management/views.py
+++ management/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.models import User
 from django.contrib.admin.views.decorators import staff_member_required
 from django.urls import reverse
@@ -47,3 +47,32 @@ def user_accounts(request):
             "users_list": users_list,
         }
     )
+
+
+@staff_member_required
+def user_activation(request, user_id):
+    """
+    Activate and deactivate user accounts
+
+    **Context**
+    ``users``
+    An instance of :model:`User`.
+
+    **Template:**
+
+    :template:`management/users.html`
+    """
+
+    user = get_object_or_404(User, pk=user_id)
+
+    print("user id:", user.id, user_id)
+    if user.is_active:
+        print("User is active, deactivate")
+        user.is_active = False
+    else:
+        print("User is inavtive, activate")
+        user.is_active = True
+
+    user.save()
+
+    return HttpResponseRedirect(reverse('users'))
