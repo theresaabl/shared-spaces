c3c126ba54e882757207b4294c72c7897a01973e
theresaabl <theresaabl@gmx.at>
2025-02-07 17:54:39 +0100

    Make separate page for user accounts in admin space, create view, template, url

--- management/urls.py
+++ management/urls.py
@@ -3,4 +3,5 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.management_page, name='management'),
+    path('user-accounts/', views.user_accounts, name='users'),
 ]
--- management/views.py
+++ management/views.py
@@ -11,11 +11,30 @@ def management_page(request):
     """
     Display the management page
 
-    **Context**
-
     **Template:**
 
     :template:`management/management_page.html`
+    """
+
+    # if request.method is GET
+    return render(
+        request,
+        "management/management_page.html",
+    )
+
+
+@staff_member_required
+def user_accounts(request):
+    """
+    Display the user accounts page
+
+    **Context**
+    ``users``
+    An instance of :model:`User`.
+
+    **Template:**
+
+    :template:`management/users.html`
     """
 
     users_list = User.objects.all()
@@ -23,7 +42,7 @@ def management_page(request):
     # if request.method is GET
     return render(
         request,
-        "management/management_page.html",
+        "management/users.html",
         {
             "users_list": users_list,
         }
