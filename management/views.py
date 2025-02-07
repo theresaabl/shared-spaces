571d10c185c5ecb08a69b05602f3a3df6e31fd2f
theresaabl <theresaabl@gmx.at>
2025-02-07 18:39:29 +0100

    Add code to make sure that user is not changing their own status

--- management/views.py
+++ management/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, get_object_or_404
+from django.contrib import messages
 from django.contrib.auth.models import User
 from django.contrib.admin.views.decorators import staff_member_required
 from django.urls import reverse
@@ -59,17 +60,25 @@ def user_activation(request, user_id):
     :template:`management/users.html`
     """
 
-    user = get_object_or_404(User, pk=user_id)
-
-    print("user id:", user.id, user_id)
-    if user.is_active:
-        print("User is active, deactivate")
-        user.is_active = False
+    # check that request.user is not accidentally changing their own status
+    if user_id == request.user.id:
+        messages.add_message(
+                    request,
+                    messages.ERROR,
+                    'You cannot deactivate your own account!'
+                )
     else:
-        print("User is inavtive, activate")
-        user.is_active = True
+        user = get_object_or_404(User, pk=user_id)
 
-    user.save()
+        print("user id:", user.id, user_id)
+        if user.is_active:
+            print("User is active, deactivate")
+            user.is_active = False
+        else:
+            print("User is inavtive, activate")
+            user.is_active = True
+
+        user.save()
 
     return HttpResponseRedirect(reverse('users'))
 
@@ -83,17 +92,24 @@ def user_admin_status(request, user_id):
 
     :template:`management/users.html`
     """
-
-    user = get_object_or_404(User, pk=user_id)
-
-    print("user id:", user.id, user_id)
-    if user.is_staff:
-        print("User is staff, remove")
-        user.is_staff = False
+    # check that request.user is not accidentally changing their own status
+    if user_id == request.user.id:
+        messages.add_message(
+                    request,
+                    messages.ERROR,
+                    'You cannot remove your own admin status!'
+                )
     else:
-        print("User is not staff, give admin")
-        user.is_staff = True
+        user = get_object_or_404(User, pk=user_id)
 
-    user.save()
+        print("user id:", user.id, user_id)
+        if user.is_staff:
+            print("User is staff, remove")
+            user.is_staff = False
+        else:
+            print("User is not staff, give admin")
+            user.is_staff = True
+
+        user.save()
 
     return HttpResponseRedirect(reverse('users'))
