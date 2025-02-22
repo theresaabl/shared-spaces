2259e0c39ed4406ee739ab487e61d2c7855aaf0a
theresaabl <theresaabl@gmx.at>
2025-02-22 11:51:57 +0100

    Register dashboard models to admin panel with admin.register decorator

--- dashboard/admin.py
+++ dashboard/admin.py
@@ -1,8 +1,54 @@
 from django.contrib import admin
 from .models import EventSpace, EventSpaceBooking, ResidentRequest
 
-admin.site.register(EventSpace)
 
-admin.site.register(EventSpaceBooking)
+@admin.register(EventSpace)
+class EventSpaceAdmin(admin.ModelAdmin):
+    """
+    Register EventSpace model to admin panel
+    Customize display, search fields and filter
+    """
+    list_display = (
+                    "name",
+                    "type",
+                    "building",
+                    "capacity",
+                    )
+    search_fields = ["name", "type"]
+    list_filter = ("type",)
 
-admin.site.register(ResidentRequest)
+
+@admin.register(EventSpaceBooking)
+class EventSpaceBookingAdmin(admin.ModelAdmin):
+    """
+    Register EventSpaceBooking model to admin panel
+    Customize display, search fields and filter
+    """
+    list_display = (
+                    "resident",
+                    "event_space",
+                    "date",
+                    "start",
+                    "end",
+                    "created_on",
+                    "status"
+                    )
+    search_fields = ["resident", "event_space", "date"]
+    list_filter = ("status",)
+
+
+@admin.register(ResidentRequest)
+class ResidentRequestAdmin(admin.ModelAdmin):
+    """
+    Register ResidentRequest model to admin panel
+    Customize display, search fields and filter
+    """
+    list_display = (
+                    "resident",
+                    "purpose",
+                    "urgent",
+                    "created_on",
+                    "status"
+                    )
+    search_fields = ["resident", "content"]
+    list_filter = ("purpose", "status")
