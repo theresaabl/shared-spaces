fca26fe42429170638654c6071d481d9937b8f38
theresaabl <theresaabl@gmx.at>
2025-02-22 11:06:56 +0100

    Register contact message to admin panel using admin.register decorator

--- contact/admin.py
+++ contact/admin.py
@@ -1,4 +1,19 @@
 from django.contrib import admin
 from .models import ContactMessage
 
-admin.site.register(ContactMessage)
+
+@admin.register(ContactMessage)
+class ContactAdmin(admin.ModelAdmin):
+    """
+    Register ContactMessage model to admin panel
+    Customize display, search fields and filter
+    """
+    list_display = (
+                    "name",
+                    "email",
+                    "interest_to_join",
+                    "sent_on",
+                    "processed"
+                    )
+    search_fields = ["name", "message"]
+    list_filter = ("processed", "interest_to_join")
--- contact/forms.py
+++ contact/forms.py
@@ -3,6 +3,16 @@ from .models import ContactMessage
 
 
 class ContactForm(forms.ModelForm):
+    """
+    Form for creating contact messages
+    Based on the ContactMessage model
+
+    Fields:
+        - name (CharField)
+        - email (EmailField)
+        - interest_to_join (BooleanField)
+        - message (TextField)
+    """
     class Meta:
         model = ContactMessage
         fields = ('name', 'email', 'interest_to_join', 'message')
