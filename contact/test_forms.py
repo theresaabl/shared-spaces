ae6aacb98f2476e0178cf1a55d36a7bf43d4b52e
theresaabl <theresaabl@gmx.at>
2025-02-22 19:50:20 +0100

    Add more tests to contact.forms and provide more detailed fail messages

--- contact/test_forms.py
+++ contact/test_forms.py
@@ -20,7 +20,7 @@ class TestContactForm(TestCase):
             'interest_to_join': 'True',
             'message': 'This is a nice message.'
             })
-        self.assertFalse(contact_form.is_valid(), msg="Form is valid")
+        self.assertFalse(contact_form.is_valid(), msg="Name not provided, but form is valid")
 
     def test_form_is_invalid_email_missing(self):
         contact_form = ContactForm({
@@ -29,7 +29,7 @@ class TestContactForm(TestCase):
             'interest_to_join': 'True',
             'message': 'This is a nice message.'
             })
-        self.assertFalse(contact_form.is_valid(), msg="Form is valid")
+        self.assertFalse(contact_form.is_valid(), msg="Email not provided, but form is valid")
 
     def test_form_is_invalid_email_invalid(self):
         contact_form = ContactForm({
@@ -38,7 +38,25 @@ class TestContactForm(TestCase):
             'interest_to_join': 'True',
             'message': 'This is a nice message.'
             })
-        self.assertFalse(contact_form.is_valid(), msg="Form is valid")
+        self.assertFalse(contact_form.is_valid(), msg="Email not an email address, but form is valid")
+
+    def test_form_is_invalid_email_invalid_dot(self):
+        contact_form = ContactForm({
+            'name': 'test name',
+            'email': 'name@test',
+            'interest_to_join': 'True',
+            'message': 'This is a nice message.'
+            })
+        self.assertFalse(contact_form.is_valid(), msg="Email missing dot, but form is valid")
+
+    def test_form_is_invalid_email_invalid_at(self):
+        contact_form = ContactForm({
+            'name': 'test name',
+            'email': 'test.com',
+            'interest_to_join': 'True',
+            'message': 'This is a nice message.'
+            })
+        self.assertFalse(contact_form.is_valid(), msg="Email missing @, but form is valid")
 
     def test_form_is_invalid_message_missing(self):
         contact_form = ContactForm({
@@ -47,4 +65,4 @@ class TestContactForm(TestCase):
             'interest_to_join': 'False',
             'message': ''
             })
-        self.assertFalse(contact_form.is_valid(), msg="Form is valid")
+        self.assertFalse(contact_form.is_valid(), msg="Message not provided, but form is valid")
