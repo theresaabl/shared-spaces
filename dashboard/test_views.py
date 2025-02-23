f9fb4e4ad4b9310ef4cd6eeabfec9360a981ae4a
theresaabl <theresaabl@gmx.at>
2025-02-23 17:22:16 +0100

    Test dashboard ResidentSpaceViews

--- dashboard/test_views.py
+++ dashboard/test_views.py
@@ -1,6 +1,8 @@
+import datetime
 from django.urls import reverse
 from django.test import TestCase
 from django.contrib.auth.models import User
+from .models import EventSpace, EventSpaceBooking, ResidentRequest
 
 
 class TestSignUpViews(TestCase):
@@ -101,7 +103,7 @@ class TestLoginViews(TestCase):
             "../templates/account/account_inactive.html"
             )
 
-    def test_active_login_successfully(self):
+    def test_active_user_login_successfully(self):
         """
         Verifies that active user can sign in and is redirected
         does not need to test further since is handled by standard allauth
@@ -112,3 +114,100 @@ class TestLoginViews(TestCase):
             )
         # Page is redirected correctly
         self.assertEqual(response.status_code, 302)
+
+
+class TestResidentSpaceViews(TestCase):
+    """
+    Test resident space views
+    Test that resident space page is rendered for authenticated users
+    Different content for not authenticated users
+    """
+    def setUp(self):
+        # Create User
+        self.user = User.objects.create_user(
+            username="testusername",
+            email="name@test.com",
+            password="testpassword",
+            is_active=True
+        )
+        # Create example event space
+        self.event_space = EventSpace.objects.create(
+            name="test space",
+            type="test type",
+            image="test image",
+            building="test building",
+            capacity="10",
+            number_of_tables="10",
+            number_of_chairs="10",
+            kitchen=False,
+            tea_and_coffeemaker=False,
+            projector=False,
+            audio_equipment=False,
+            childrens_play_area=False,
+            piano=False,
+            notes="test notes"
+            )
+        # Create event space bookings
+        self.booking = EventSpaceBooking(
+            resident=self.user,
+            event_space=self.event_space,
+            occasion="test occasion",
+            date="2025-10-18",
+            start=datetime.datetime.strptime('19:00', '%H:%M').time(),
+            end=datetime.datetime.strptime('22:00', '%H:%M').time(),
+            notes="test notes",
+            created_on=datetime.datetime.today(),
+            status="0"
+            )
+        self.booking.save()
+
+        # Create resident requests
+        self.res_maint_request = ResidentRequest(
+            resident=self.user,
+            purpose="0",
+            urgent=True,
+            content="test maintenance request",
+            created_on=datetime.datetime.today(),
+            status="0"
+            )
+        self.res_maint_request.save()
+
+        self.res_message = ResidentRequest(
+            resident=self.user,
+            purpose="1",
+            urgent=False,
+            content="test message",
+            created_on=datetime.datetime.today(),
+            status="2"
+            )
+        self.res_message.save()
+
+    def test_render_content_for_authenticated_users(self):
+        self.client.login(username="testusername", password="testpassword")
+        response = self.client.get(reverse('dashboard'))
+        # Page is rendered correctly
+        self.assertEqual(response.status_code, 200)
+        # Check content for logged in users
+        self.assertIn(b"Welcome to Your Resident Space", response.content)
+        self.assertIn(b"testusername", response.content)
+
+        # Check that event space bookings are displayed
+        self.assertIn(b"test space", response.content)
+        self.assertIn(b"test occasion", response.content)
+
+        # Check that resident requests are displayed
+        # For urgent maintenance request
+        self.assertIn(b"Maintenance Request", response.content)
+        self.assertIn(b"test maintenance request", response.content)
+        self.assertIn(b"Urgent!", response.content)
+        # For message
+        self.assertIn(b"Message", response.content)
+        self.assertIn(b"test message", response.content)
+
+    def test_render_content_for_unauthenticated_users(self):
+        response = self.client.get(reverse('dashboard'))
+        # Page is rendered correctly
+        self.assertEqual(response.status_code, 200)
+        # Check content for not logged in users
+        self.assertIn(b"You are not signed in !", response.content)
+
